763b6ed8d19c04f53ff7e9ec07e8253e4ea8b022
dedyalfaris <dedyalfaris.da41@gmail.com>
2021-12-01 23:56:23 +0700

    Tugas 4 C++
    Berikut saya lampirkan Tugas 4 C++ Saya kepada Niomic

---
+++ tugas4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x=30;
+    cout<<"Nilai Awal X : "<<x<<endl;
+    x--;
+    cout<<"Nilai Kedua X : "<<x<<endl;
+    x+=2;
+    cout<<"Nilai Ketiga X : "<<x<<endl;
+
+    
+
+}
